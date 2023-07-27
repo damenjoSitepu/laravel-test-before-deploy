789832e19bca74d1c31621f56a9dd8d1716fcc12
Damenjo Sitepu <damenjos@thebigrichgroup.com>
2023-07-27 18:38:57 +0700

    Setup: First route test using integration test

---
+++ tests/Feature/RouteTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RouteTest extends TestCase
+{   
+    /**
+     * Route Test
+     * 
+     * @test
+     */
+    public function routeTest(): void
+    {
+        $response = $this->get("/dashboard");
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Route Test For Profile
+     * 
+     * @test 
+     */
+    public function routeProfileTest()
+    {
+        $response = $this->get("profile");
+        $response->assertStatus(200);
+    }
+}
