a94a811b237f69abf67ebfdc4864117bee38d4cc
Damenjo Sitepu <damenstory@gmail.com>
2023-07-27 19:06:24 +0700

    Update: Duplication or delete for user test

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Models\User;
 use Tests\TestCase;
 
 class UserTest extends TestCase
@@ -15,5 +16,41 @@ class UserTest extends TestCase
     {
         $response = $this->get('/login');
         $response->assertStatus(200);
+    }
+
+    /**
+     * Test Delete User Functionality
+     *
+     * @test
+     */
+    public function userDelete()
+    {
+        $createUser = User::factory()->count(1)->make();
+        $findUser = User::first();
+        // Make sure that user exists
+        if ($findUser) {
+            $findUser->delete();
+        }
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Test For User Duplication
+     *
+     * @test
+     */
+    public function userDuplication()
+    {
+        $userA = User::make([
+            'name' => 'Damenjo Sitepu',
+            'email' => 'damenjo@gmail.com'
+        ]);
+
+        $userB = User::make([
+            'name' => 'Tari Puspita Sari',
+            'email' => 'tari@gmail.com'
+        ]);
+        
+        $this->assertTrue($userA != $userB);
     }
 }
